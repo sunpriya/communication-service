84b22ba693bcac0ab0b7b654b369d17f45e4fde4
sunpriya <sunpriyakaurbhatia@gmail.com>
2016-11-05 12:41:48 +0530

    Create constants.py

---
+++ sunpriya/constants.py
@@ -0,0 +1,10 @@
+API_PATH_PREFIX="/communicatiom"
+API_PATH_EMAIL="/email_id"
+ESPONSE_MESSAGE_UNAUTHORIZED = "Authentication error occurred"
+RESPONSE_MESSAGE_NOT_FOUND = "The requested resource was not found"
+RESPONSE_MESSAGE_BAD_REQUEST = "There was a problem with your request"
+RESPONSE_MESSAGE_NOT_IMPLEMENTED = "The requested resource is not implemented"
+
+DB_NAME = "communication-service"
+
+EMAIL_ID_COLLECTION = "email-Id-collection"
